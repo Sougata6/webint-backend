1f3d49509aa78b9723557bbb7cb432c94246a1de
Sougata6 <janasougata6@gmail.com>
2025-03-23 20:48:30 +0530

    Removed questions

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import cors from 'cors';
 import { authRoutes } from './routes/auth.js';
 import { userRoutes } from './routes/user.js';
-import { questionRoutes } from './routes/questions.js';
 
 dotenv.config();
 const app = express();
@@ -15,7 +14,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
-app.use('/questions', questionRoutes);
 
 app.get('/ping', (req, res) => {
   res.status(200).json({
--- constants/collections.js
+++ constants/collections.js
@@ -2,7 +2,6 @@
 export const DB_NAME =  process.env.DB_NAME || 'webint-dev';
 
 export const USERS_COLL = 'users';
-export const QUESTIONS_COLL = 'questions';
 export const CATEGORIES_COLL = 'categories';
 export const PASSWORD_COLL = 'passwords';
 export const REFRESH_TOKEN_COLL = 'refreshTokens';
